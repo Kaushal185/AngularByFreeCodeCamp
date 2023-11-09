bb7bd3b0a91b4bb7360984c7c973bbe984fa7f0f
Kaushal Kawade <kaushal.kawade@oracle.com>
2023-11-09 14:10:12 +0530

    angular scss project is created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  // template: '<h1>Hello wORLD<h1>
+  //styles:'h1{color.red}'
 })
 export class AppComponent {
   title = 'hotelinventry';
